436d61e7cdb4125aa1e1c94e7543eaff2488b192
Miguel Carballo <miguelcarballoglez@gmail.com>
2024-03-25 13:11:43 +0000

    Register companie view

--- backend/companies/views.py
+++ backend/companies/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth.tokens import default_token_generator
+from django_otp.plugins.otp_totp.models import TOTPDevice
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_protect
+from django.template.loader import render_to_string
+from rest_framework.permissions import AllowAny
+from django.utils.encoding import force_bytes
+from django.utils.encoding import force_str
+from django.middleware.csrf import get_token
 
-# Create your views here.
+
+from django.http import JsonResponse
+from .models import Empresa
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_empresa_view(request):
+    if request.method == 'POST':
+        data = request.data
+        try:
+            empresa = Empresa.objects.create_empresa(nombre=data['nombre'], codigo_empresa=data['codigo_empresa'], cif=data['cif'], direccion=data['direccion'])
+        except Exception as e:
+            return JsonResponse({'errors': str(e)}, status=400)
+
+       
+        try:
+            empresa = Empresa(nombre=data['nombre'], codigo_empresa=data['codigo_empresa'], cif=data['cif'], direccion=data['direccion'])
+            empresa.save()
+        except Exception as e:
+            empresa.delete() 
+            return JsonResponse({'errors': str(e)}, status=400)
+
+       
+
+    else:
+        return JsonResponse({'error': 'Invalid Request'}, status=405)
--- backend/sportevent/urls.py
+++ backend/sportevent/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('users/', include('users.urls')),  
+    path('companies/', include('companies.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
