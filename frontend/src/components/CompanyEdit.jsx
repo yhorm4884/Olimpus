310b440a7f40a49e6d6f59df874a9005ac45cf3e
Miguelcg03 <miguelcarballoglez@gmail.com>
2024-05-14 18:07:40 +0000

    Subida de Olimpus

--- frontend/src/components/CompanyEdit.jsx
+++ frontend/src/components/CompanyEdit.jsx
@@ -2,11 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { Avatar, TextField, Button, Paper, Typography, IconButton } from '@mui/material';
 import { Save as SaveIcon, PhotoCamera as PhotoCameraIcon } from '@mui/icons-material';
-import CompanyDetails from './CompanyDetails';
 
 const CompanyEdit = ({ userId }) => {
-  const [company, setCompany] = useState({ nombre: '', estado: true, photo: '', ubicacion: '' });
-  const fileInputRef = useRef(null);
+  const [company, setCompany] = useState({ nombre: '', estado: true, photo: '', ubicacion: '', descripcion: '' });
 
   useEffect(() => {
     axios.get(`https://backend.olimpus.arkania.es/companies/empresa/${userId}/`)
@@ -36,21 +34,18 @@ const CompanyEdit = ({ userId }) => {
     formData.append('nombre', company.nombre);
     formData.append('estado', company.estado);
     formData.append('ubicacion', company.ubicacion);
+    formData.append('descripcion', company.descripcion); // Add descripcion to the formData
     formData.append('photo', company.photo);
     axios.post(`https://backend.olimpus.arkania.es/companies/empresa/${userId}/`, formData)
       .then(() => alert('Empresa actualizada con éxito'))
       .catch(err => alert('Error al actualizar la empresa: ' + err.message));
   };
 
-  const handleAvatarClick = () => {
-    fileInputRef.current.click();
-  };
-  
   return (
     <Paper style={{ padding: 20, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Typography variant="h6" style={{ alignSelf: 'start' }}>Editar Empresa</Typography>
       <IconButton onClick={handleAvatarClick} style={{ margin: '20px' }}>
-        <Avatar src={company.photo} style={{ width: 90, height: 90 }} />
+        <Avatar src={company.photo} style={{ width: 90, height  90 }} />
         <PhotoCameraIcon style={{ position: 'absolute', color: 'rgba(255, 255, 255, 0.7)' }} />
       </IconButton>
       <input
@@ -77,7 +72,18 @@ const CompanyEdit = ({ userId }) => {
           variant="outlined"
           fullWidth
           margin="normal"
-          value={company.direccion || ''}
+          value={company.ubicacion || ''}
+          onChange={handleInputChange}
+        />
+        <TextField
+          label="Descripción de la empresa"
+          name="descripcion"
+          variant="outlined"
+          fullWidth
+          multiline
+          rows={4}
+          margin="normal"
+          value={company.descripcion || ''}
           onChange={handleInputChange}
         />
         <div>
@@ -107,3 +113,4 @@ const CompanyEdit = ({ userId }) => {
 };
 
 export default CompanyEdit;
+
